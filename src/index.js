5386d45cfec558d24505aac29545fa915fe682c3
rguerraapaza <rguerraapaza@gmail.com>
2020-02-17 21:38:38 -0500

    proyecto de validacion de tarjeta de credito

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+const btnCheck = document.getElementById("btn_check");
+
+btnCheck.addEventListener('click', () => {
+    
+    let nambercreditcard = document.getElementById('namber_creditcard').value;
+    let rev_nambercreditcard = (nambercreditcard.split('').reverse());
+    //console.log('el reverso es: '+ rev_nambercreditcard);
+    let tarjetaValida = validator.isValid(rev_nambercreditcard);
+
+    let tarjetaenmascarada = validator.maskify(nambercreditcard);
+    console.log('enmascarada',tarjetaenmascarada);
+    
+
+    //CONTENEDORES
+    const cont1 = document.getElementById('contenedor1');
+    const cont2 = document.getElementById('contenedor2');
+    cont2.style.display = 'none';
+
+    //*********************************************** */
+    //SI SON MENORES QUE 16 DIGITOS
+    console.log(nambercreditcard);
+    const info = document.getElementById("saludo");
+    info.innerHTML = 'Hola tu numero de tarjeta es: '+ tarjetaenmascarada;
+    //info.innerHTML = 'Hola tu numero de tarjeta es: ############'+cadena;
+    console.log(info);
+    cont2.style.display = 'block';
+    cont1.style.display = 'none'; 
+    if (tarjetaValida == true) {
+        alert('es valida');
+    }
+    else{
+        alert('no es valida');
+    }  
+    
+})
+
+/*
+//REEMPLAZAR CON CARACTERES DE NUMERAL '#' - USANDO SLICE
+let cadena = nambercreditcard.slice(12,16);
+console.log( '############'+cadena); 
+
+//PROBANDO EL USO DEL SPLIT
+let separar = nambercreditcard.split('');
+console.log(separar);
+*/
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,73 @@
 const validator = {
-  // ...
+  isValid : (creditCardNumber) => {
+       //&& creditCardNumber.length>=1
+    if (creditCardNumber.length <= 16 ) {
+      let suma = 0;
+      let n = 0;
+      for (let i = 0; i <creditCardNumber.length; i++) {
+        /*         
+        const element = creditCardNumber[i];        
+        console.log('['+ i + '] -> ' + element);*/
+        if (i%2 == 0) {
+            
+            n = (creditCardNumber[i]- '0');
+        }
+        else
+        {
+            n = (creditCardNumber[i]-'0')*2
+            //console.log('(*2) ->' + n);
+            if (n >= 10) {
+                n = (n - 10) + 1;
+                console.log('sum>10 ->'+n);
+            }
+        }
+        suma = suma + n;  
+        //console.log(suma);     
+      }
+      if (suma % 10 == 0) {
+        console.log('valida ' + suma);
+        let conf_validacion = document.getElementById('accepted');
+        
+        //conf_validacion.innerHTML = 'La tarjeta '+ type_card[i].value +' es valida ' + suma;
+        conf_validacion.innerHTML = 'La tarjeta es valida ' + suma;
+        return true;
+      }
+      else
+      {
+          console.log('no es valida ' + suma);
+          const conf_validacion2 = document.getElementById('rejected');
+          conf_validacion2.innerHTML = 'La tarjeta no es valida '+ suma;
+          return false;
+      }
+    } 
+    else 
+    {
+      const alert = document.getElementById("alert_creditcard");
+      alert.innerHTML = 'falta caracteres solo hay '+ creditCardNumber.length;
+      console.log(alert);
+    }
+    
+  },
+
+  maskify : (creditCardNumber) => {
+    //CONVIRTIENDO 
+    let numero_convertido = creditCardNumber.toString().split('');
+    
+    for (let i = numero_convertido.length - 1; i >= 0; i--) {
+      
+      if (i > numero_convertido.length - 5) {
+          
+        console.log( numero_convertido[i]);
+      }
+      else
+      {
+        numero_convertido[i] = '#';          
+      }         
+    }
+    let juntaNumeros = numero_convertido.join('');  
+    console.log('numero es: '+ juntaNumeros);  
+    return juntaNumeros;
+  }
 };
 
 export default validator;
